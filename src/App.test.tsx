df3a280b40d2dfe367362939d37178fb35ffaa63
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2021-09-06 15:19:23 +0300

    CD

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders You Must Hear', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('You Must Hear');
   expect(linkElement).toBeInTheDocument();
 });
